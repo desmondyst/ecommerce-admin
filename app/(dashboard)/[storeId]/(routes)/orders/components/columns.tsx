b9fbba08d10cc22c5bde2a4a0ad631355eb31bc7
desmondyst <desmondyong06@gmail.com>
2023-11-30 18:17:04 +0800

    :bug: fix bug

--- app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
-import { CellAction } from "./cell-action";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -16,7 +15,7 @@ export type OrderColumn = {
     createdAt: string;
 };
 
-export const columns: ColumnDef<BillboardColumn>[] = [
+export const columns: ColumnDef<OrderColumn>[] = [
     // NOTE: Specifies the key (property) in the data object from which to retrieve the data for this column. In this case, it's "label."
     {
         accessorKey: "products",
