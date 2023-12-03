4eb16fba1a4705718eb2748c68346ae851f9b808
desmondyst <desmondyong06@gmail.com>
2023-12-03 19:50:51 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -21,7 +21,10 @@ const nextConfig = {
                 source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "https://ecommerce-store-front-page.vercel.app/",
+                    },
                     {
                         key: "Access-Control-Allow-Methods",
                         value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
